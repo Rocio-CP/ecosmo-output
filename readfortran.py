d68373de06f1634e93db6785ce6fd5a25adaf4f1
Rocio-CP <rocio.primo@uib.no>
2023-01-08 22:41:29 +0100

    it does read a biomass file. Have to check if it read it correctly

--- readfortran.py
+++ readfortran.py
@@ -1,6 +1,7 @@
 # BASE IT ON READ_BIOMASS
 from scipy.io import FortranFile
 import numpy as np
+import matplotlib.pyplot as plt
 from itertools import islice
 
 files_dir='/Users/rocio/Dropbox/ECOSMO/ecosmosourcefiles/'
@@ -10,11 +11,12 @@ m=177
 n=207
 ilo=20
 nphys=20
-nbio=23
+nbio=23 # 21 in-file, total phyto and zoo calculated later
 nprod=25
 
-khor=8216; khor1=khor+1
-ndrei=82108
+khor=8216 # number surface wet points
+khor1=khor+1
+ndrei=82108 # total number of wet points
 kasor=8106
 
 
@@ -44,6 +46,7 @@ dlr,rdln,dlvo,dlvu,*rest= np.split(g3, np.cumsum(seclist))
 
 
 # Read the file; iwet points per line, each new line is a day
+# all points per variable together, or all variables per point??
 f = FortranFile( files_dir+'NN09401b', 'r' )
 fdata=f.read_reals(dtype='float32')
 
@@ -56,16 +59,31 @@ fdata=f.read_reals(dtype='float32')
 u=np.empty([m,n,ilo])
 u[:]=np.nan
 
-lwe = -1
-nwet = -1
-for j in range(0,n-1):
+lwe = -1 # lwe??
+nwet = -1 # nwet = total number of wet points. In 3d. Counter, locator in fdata
+#for j in range(0,n-1): # going W-E per point
+#    lwa = lwe + 1
+#    lwe = indend[j] - 1 # indend: number of N-W AND depth (?) points at j-location
+#    for lw in range(lwa,lwe):
+#        i = iwet[lw] -1 # iwet: i-index of wet surface points
+#        lump = lazc[lw] -1
+#        for k in range(0,lump-1):
+#            nwet = nwet + 1
+#            u[i, j, k] = fdata[nwet] -1
+
+
+for j in range(0,5):#n-1):
     lwa = lwe + 1
-    lwe = indend[j] - 1
+    lwe = indend[j] - 1 # indend is cumulative
+    print("j,lwa,lwe"+[j,lwa,lwe])
     for lw in range(lwa,lwe):
         i = iwet[lw] -1
         lump = lazc[lw] -1
+        print(lw,i,lump)
         for k in range(0,lump-1):
             nwet = nwet + 1
-            u[i, j, k] = fdata[nwet] -1
+            u[i, j, k] = fdata[nwet] #-1
+            print(k,fdata[nwet])
 
+#plt.imshow(u[:,:,0])
 
